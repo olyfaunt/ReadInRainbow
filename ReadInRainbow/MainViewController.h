c6d4186d1b98ee1c9ef16d59889bfb8160e034dd
Audrey Jun <audrey.jun@gmail.com>
2014-11-17 14:12:36 -0800

    Added Main View Controller class and in Storyboard

---
+++ ReadInRainbow/MainViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  ReadInRainbow
+//
+//  Created by Audrey Jun on 2014-11-17.
+//  Copyright (c) 2014 ajek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GameCell.h"
+#import "ChartCell.h"
+
+@interface MainViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *chartCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *gamesCollectionView;
+
+@end
+
