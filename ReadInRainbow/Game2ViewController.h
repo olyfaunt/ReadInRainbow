8ef0c1a99893ea30d0fc6975869796e46394afeb
kardar233 <kauffmanelia@gmail.com>
2014-11-19 11:16:26 -0800

    Added Game 2 data and access from main

---
+++ ReadInRainbow/Game2ViewController.h
@@ -0,0 +1,15 @@
+//
+//  Game2ViewController.h
+//  ReadInRainbow
+//
+//  Created by Elia Kauffman on 2014-11-19.
+//  Copyright (c) 2014 ajek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Game2ViewController : UIViewController
+
+@property (nonatomic, strong) NSArray * soundsArray;
+
+@end
