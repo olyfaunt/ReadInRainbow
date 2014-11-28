ca35aacc2674226796577317e318acc9bae39e5e
kardar233 <kauffmanelia@gmail.com>
2014-11-28 13:51:27 -0800

    Made all the transitions between the view controllers nice.

---
+++ ReadInRainbow/MainMenuReturnProtocol.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuReturnProtocol.h
+//  ReadInRainbow
+//
+//  Created by Elia Kauffman on 2014-11-28.
+//  Copyright (c) 2014 ajek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MainMenuReturnProtocol <NSObject>
+
+@end
